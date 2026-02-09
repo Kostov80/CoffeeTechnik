3f1922fd30747541c10bd7ee121c57ec0565b55b
Ilian Kostov <ilian.kostov.ff@gmail.com>
2026-02-09 20:56:19 +0200

    Add LoginViewModel, AccountController and Login view

---
+++ CoffeeTechnik/Controllers/AccountController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using CoffeeTechnik.Models;
+
+namespace CoffeeTechnik.Controllers
+{
+    public class AccountController : Controller
+    {
+        [HttpGet]
+        public IActionResult Login()// Показване на  входа
+        {
+         
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(LoginViewModel model)//обработване на входа
+        {
+            if (!ModelState.IsValid)
+            {
+                return  View(model);
+            }
+
+            
+            if (model.Username == "tech" && model.Password == "1234")// Проверка на потребителското име и паролата 
+            {
+                return RedirectToAction("Index", "Home");
+            }
+
+            ModelState.AddModelError("", "Грешно потребителско име или парола");// Грешно потребителско име или парола
+            return View(model);
+        }
+
+        [HttpPost]
+        public IActionResult GuestLogin()
+        {
+            
+             return RedirectToAction("Index", "Home");// Влизане като гост 
+        }
+    }
+}
---
+++ CoffeeTechnik/Models/LoginViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CoffeeTechnik.Models
+{
+    public class LoginViewModel// Модел за вход
+    {
+        [Required]
+        public string Username { get; set; }//Име на техника
+
+        [Required]
+        [DataType(DataType.Password)]//Парола
+        public string Password { get; set; }
+
+        public bool RememberMe { get; set; }//Запомня сесията
+    }
+}
