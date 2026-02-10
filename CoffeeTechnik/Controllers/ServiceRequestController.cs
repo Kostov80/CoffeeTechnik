1df8a7b637dfd2960dccd1047f48098102265b30
Ilian Kostov <ilian.kostov.ff@gmail.com>
2026-02-10 15:28:13 +0200

    installation page

--- CoffeeTechnik/Controllers/ServiceRequestController.cs
+++ CoffeeTechnik/Controllers/ServiceRequestController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using CoffeeTechnik.Models;
 
 namespace CoffeeTechnik.Controllers
 {
@@ -7,40 +8,61 @@ namespace CoffeeTechnik.Controllers
         [HttpGet]
         public IActionResult Create()
         {
-            return View();
+            
+            return View();//начална страница 
         }
 
         
-        public IActionResult CreateMontage()
+        [HttpGet]
+        public IActionResult CreateMontage()// станица за монтаж
         {
-            ViewData["RequestType"] = "Монтаж";
-           
-            return View();
+            ViewData["PageTitle"] = "Страница за Монтаж";
+
+            return View(new MontageViewModel()); 
         }
 
         
+        [HttpPost]
+        public IActionResult CreateMontage(MontageViewModel model) //обработка на данните 
+        {
+            ViewData["PageTitle"] = "Страница за Монтаж";
+
+            if (!ModelState.IsValid)
+            {
+                
+                return View(model);//грешка при празно поле
+            }
+
+            
+            TempData["SuccessMessage"] = "Заявката е запаметена успешно!";
+
+            
+            return RedirectToAction("Create");// след запазване начална страница
+        }
+
+        
+        [HttpGet]
         public IActionResult CreateDemontage()
         {
             ViewData["RequestType"] = "Демонтаж";
-           
-            
+
             return View();
         }
 
-        
+        [HttpGet]
         public IActionResult CreateEmergency()
         {
             ViewData["RequestType"] = "Авария";
+
             return View();
         }
 
-        
+        [HttpGet]
         public IActionResult CreateMaintenance()
         {
             ViewData["RequestType"] = "Профилактика";
-           
+
             return View();
         }
-
     }
 }
---
+++ CoffeeTechnik/Models/MontageViewModel.cs
@@ -0,0 +1,50 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CoffeeTechnik.Models
+{
+    public class MontageViewModel
+    {
+        [Required(ErrorMessage = "Моля, въведете име на обекта")]
+        public string ObjectName
+        { get; set; }
+
+
+        [Required(ErrorMessage = "Моля, въведете булстат")]
+        public string BULSTAT 
+        { get; set; }
+
+
+        [Required(ErrorMessage = "Моля, въведете град")]
+        public string City 
+        { get; set; }
+
+
+        [Required(ErrorMessage = "Моля, въведете адрес")]
+        public string Address
+        { get; set; }
+
+
+        [Required(ErrorMessage = "Моля, въведете човек за контакт")]
+        public string ContactPerson
+        { get; set; }
+
+
+        [Required(ErrorMessage = "Моля, въведете телефон")]
+        [Phone(ErrorMessage = "Невалиден телефонен номер")]
+        public string Phone 
+        { get; set; }
+
+
+        [Required(ErrorMessage = "Моля, въведете модел на машината")]
+        public string MachineModel 
+        { get; set; }
+
+        [Required(ErrorMessage = "Моля, свържете машината")]
+        public string MachineConnection 
+        { get; set; }
+
+        [Required(ErrorMessage = "Моля, въведете локация")]
+        public string Location 
+        { get; set; }
+    }
+}
