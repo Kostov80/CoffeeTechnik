fe5946783a3b75ce836b160e5b4c512249a7aaca
Ilian Kostov <ilian.kostov.ff@gmail.com>
2026-02-10 13:55:45 +0200

    add views

---
+++ CoffeeTechnik/Controllers/ServiceRequestController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CoffeeTechnik.Controllers
+{
+    public class ServiceRequestController : Controller
+    {
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        
+        public IActionResult CreateMontage()
+        {
+            ViewData["RequestType"] = "Монтаж";
+           
+            return View();
+        }
+
+        
+        public IActionResult CreateDemontage()
+        {
+            ViewData["RequestType"] = "Демонтаж";
+           
+            
+            return View();
+        }
+
+        
+        public IActionResult CreateEmergency()
+        {
+            ViewData["RequestType"] = "Авария";
+            return View();
+        }
+
+        
+        public IActionResult CreateMaintenance()
+        {
+            ViewData["RequestType"] = "Профилактика";
+           
+            return View();
+        }
+
+    }
+}
