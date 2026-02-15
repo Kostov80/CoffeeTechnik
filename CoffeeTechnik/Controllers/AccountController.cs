e3e69d1e430c3cdbe111644e976912b9532749f0
Ilian Kostov <ilian.kostov.ff@gmail.com>
2026-02-15 13:40:26 +0200

    add nav bar

--- CoffeeTechnik/Controllers/AccountController.cs
+++ CoffeeTechnik/Controllers/AccountController.cs
@@ -5,60 +5,64 @@ namespace CoffeeTechnik.Controllers
 {
     public class AccountController : Controller
     {
+        
+
         [HttpGet]
-        public IActionResult Login() // Показване на входа
+        public IActionResult Login()   // GET заявка за показване на формата за вход
         {
-           
+
             return View();
         }
 
         [HttpPost]
-        public IActionResult Login(LoginViewModel model) // Обработване на входа
+        public IActionResult Login(LoginViewModel model)
         {
+
             if (!ModelState.IsValid)
             {
-               
                 return View(model);
             }
 
-            
-            if (model.Username == "tech" && model.Password == "1234")// Проверка на  име и парола
+            if (model.Username == "tech" && model.Password == "1234")
             {
                 TempData["UserRole"] = "Technician";
+
                 TempData.Keep("UserRole");
 
                 return RedirectToAction("Index", "Home");
             }
-
             else if (model.Username == "sales" && model.Password == "1234")
             {
-
                 TempData["UserRole"] = "Sales";
+
                 TempData.Keep("UserRole");
 
                 return RedirectToAction("Index", "Home");
             }
 
             ModelState.AddModelError("", "Грешно потребителско име или парола");
+
             return View(model);
         }
 
+        
+
         [HttpGet]
-        public IActionResult GuestLogin()// Влизане като гост
+        public IActionResult GuestLogin()// GET заявка за вход като гост
         {
-            
             TempData["UserRole"] = "Guest";
+
             TempData.Keep("UserRole");
 
             return RedirectToAction("Index", "Home");
         }
 
-        [HttpPost]
+        
+
+        [HttpGet] 
         public IActionResult Logout()
         {
-            
-            TempData["UserRole"] = "Guest";
-            TempData.Keep("UserRole");
+            TempData.Clear(); 
 
             return RedirectToAction("Login", "Account");
         }
